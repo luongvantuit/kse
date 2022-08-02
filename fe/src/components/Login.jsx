ea4edd0ea6b4c09ce592818ecc48d9d446968e25
lanhcrab <lanhpham301295@gmail.com>
2022-08-02 23:15:34 +0700

    login

--- fe/src/components/Login.jsx
+++ fe/src/components/Login.jsx
@@ -1,15 +1,51 @@
 import React from 'react'
+import './Login.css';
+import Logo from '../image/image_logo_bts.PNG';
+
 
 export default function Login() {
+  
   return (
-    <React.Fragment>
-        <div className="w-full flex justify-center items-center">
-            <form action="submit" className="flex flex-col justify-center items-center w-[300px] bg-black" onSubmit={(event)=>{
+    <React.Fragment> 
+        <div className="login-background">
+          <div className='login-container'>
+            <img src={Logo} alt='logo' className='logo'/>
+            <h6 className='jss6'>HYBE Co.</h6>
+          </div>
 
-            }}>
-                <p>Username/email</p>
-                <input type="text" />
-            </form>
+          <div className='login-content'>
+            <div className='text-center'>Chào mừng trở lại!</div>
+            
+            <div className='text-username'>
+              <p>Tên đăng nhập/E-mail</p>
+              <input type="text" 
+              className="rectangle" 
+              />
+            </div>
+
+            <div className='text-password'>
+              <p>Mật khẩu</p>
+              <input type="password" className="rectangle" />
+
+              <div className='text-forgot'>
+                <span>Quên mật khẩu?</span>
+              </div>
+            
+            </div>
+
+            
+
+            <div className='btn-rectangle'>
+              <button className='btn-login'>ĐĂNG NHẬP</button>
+            </div>
+            
+
+          
+          
+          
+          </div>
+          
+            
         </div>
     </React.Fragment>
   )
