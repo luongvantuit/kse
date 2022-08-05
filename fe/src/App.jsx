18f6c007622e6e9fa8c31a5b92ee077442812962
Ngan3108 <dangthikimngan_t64@hus.edu.vn>
2022-08-05 22:56:39 +0700

    add homepage, personnel, timesheets

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './index.css';
-import Requests from './components/Requests';
-
+//import Requests from './components/Requests';
+//import Homepage from './components/Homepage';
+//import Personnel from './components/Personnel';
 //import Login from './components/Login';
-
+import Timesheets from './components/Timesheets';
 //import Calendar from './components/Calendar';
 function App() {
   return (
-     <Requests />
+     <Timesheets/>
   );
 }
 
