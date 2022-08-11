12c9fa74df4f817fd24fd79c6429a8c0ed029592
lanhcrab <lanhpham301295@gmail.com>
2022-08-11 20:32:59 +0700

    update personallInfo

--- fe/src/components/PersonalInformation.jsx
+++ fe/src/components/PersonalInformation.jsx
@@ -5,7 +5,11 @@ export default function PersonalInformation() {
         <div className="personal-inf">
             <span className="personal-inf-name">Hồ sơ cá nhân</span>
 
-            <div className=""></div>
+            <div className="body-personal-inf-two">
+                <div className="body-personal-inf-left">
+                    
+                </div>
+            </div>
 
         </div>
 
