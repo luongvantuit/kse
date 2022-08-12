2b8a853953d6e9fdcf3da8023c50d44b3eb877e7
Ngan3108 <dangthikimngan_t64@hus.edu.vn>
2022-08-13 00:35:15 +0700

    update PersonalInfomation

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -3,24 +3,23 @@ import { Routes, Route } from "react-router-dom";
 
 import "./index.css";
 
-//import Login from "./pages/Login";
-//import HomePage from "./pages/Homepage";
-//import Requests from "./pages/Requests";
-//import Personnel from "./pages/Personnel";
+import Login from "./pages/Login";
+import HomePage from "./pages/Homepage";
+import Requests from "./pages/Requests";
+import Personnel from "./pages/Personnel";
 import AddNew from "./pages/AddNew";
 
 function App() {
   return (
-    // <React.Fragment>
-    //   <Routes>
-    //     <Route path="login" element={<Login />} />
-    //     <Route path="homepage" element={<HomePage />} />
-    //     <Route path="requests" element={<Requests />} />
-    //     <Route path="personnel" element={<Personnel />} />
-    //     <Route path="addnew" element={<AddNew />} />
-    //   </Routes>
-    // </React.Fragment>
-    <AddNew/>
+    <React.Fragment>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="homepage" element={<HomePage />} />
+        <Route path="requests" element={<Requests />} />
+        <Route path="personnel" element={<Personnel />} />
+        <Route path="addnew" element={<AddNew />} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
